9852a13da7e34bb2982df032b1ecbd3dd4571693
dvkt <c@dvkt.io>
2020-01-04 16:24:22 -0800

    more colors

--- src/color.rs
+++ src/color.rs
@@ -20,6 +20,15 @@ color!(Magenta, 95);
 color!(Cyan, 96);
 color!(White, 97);
 
+color!(DarkBlack, 30);
+color!(DarkRed, 31);
+color!(DarkGreen, 32);
+color!(DarkYellow, 33);
+color!(DarkBlue, 34);
+color!(DarkMagenta, 35);
+color!(DarkCyan, 36);
+color!(DarkWhite, 37);
+
 color!(Reset, 0);
 color!(Bold, 1);
 color!(Underline, 4);
